07ed95eaac886a292aa82389916e08a1d1d88f25
Raquel Lopez <raquelcle@gmail.com>
2026-02-17 22:49:48 -0400

    Add user management with Hono: implement user controller, model, and routes

---
+++ 1_code_first/assignment/practice/backend/src/routes/userRoutes.ts
@@ -0,0 +1,10 @@
+import { Hono } from 'hono'
+import { UserController } from '../controllers/userController'
+
+const userRoutes = new Hono()
+
+userRoutes.post('/new', UserController.createUser)
+userRoutes.post('/edit/:userId', UserController.editUser)
+userRoutes.get('/', UserController.getUser)
+
+export default userRoutes
