c4845caad6b5cdb6d962a157f8413f76da7dd10e
Raquel Lopez <raquelcle@gmail.com>
2026-03-02 22:32:42 -0400

    green(fizzbuzz): first test passes. Always returns a string

---
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export function fizzbuzz(str: string): string {
+    return ""
+}
--- 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,8 +1,9 @@
 import { describe, expect, it } from "bun:test";
+import {fizzbuzz} from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
     it("always returns a string", () => {
-        const res = fizzbuzz("a");
+        const res = fizzbuzz("string");
         expect(typeof res).toBe("string");
     })
 });
