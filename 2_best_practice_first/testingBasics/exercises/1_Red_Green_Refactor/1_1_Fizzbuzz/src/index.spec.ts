d658278485344625ebaeb8f188ed09fa2cfcb33a
Raquel Lopez <raquelcle@gmail.com>
2026-03-02 22:42:16 -0400

    red: add test that outputs 'FizzBuzz' if input is 15

--- 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -2,16 +2,20 @@ import { describe, expect, it } from "bun:test";
 import {fizzbuzz} from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-    it("always returns a string", () => {
+    it("should always return a string", () => {
         const res = fizzbuzz(2);
         expect(typeof res).toBe("string");
     });
-    it("outputs 'fizz' if the input number is 3", () => {
+    it("should output 'fizz' if the input number is 3", () => {
         const res = fizzbuzz(3)
         expect(res).toBe("fizz");
     })
-    it("outputs 'buzz' if the input number is 5", () => {
+    it("should output 'buzz' if the input number is 5", () => {
         const res = fizzbuzz(5)
         expect(res).toBe("buzz");
     })
+    it('should output "FizzBuzz" if the input number is 15', () => {
+        const res = fizzbuzz(15)
+        expect(res).toBe("FizzBuzz");
+    });
 });
