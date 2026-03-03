b6c20116a3c1548c415a43880a2f0e1a3a99f068
Raquel Lopez <raquelcle@gmail.com>
2026-03-02 23:09:39 -0400

    green: test passes that outputs an error message if input is 102

--- 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,9 +1,13 @@
 export function fizzbuzz(input: number): string {
-    if(input === 5) {
-        return 'buzz'
+    if(input >= 100 ) {
+        return 'error: input must be less than 100'
     }
+
     if(input % 15 === 0) {
         return 'FizzBuzz'
+    }
+    if(input === 5) {
+        return 'buzz'
     }
     if(input % 3 === 0) {
         return 'fizz'
