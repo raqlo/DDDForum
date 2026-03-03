9bce55deabdc6374f00ba8f5f7fcbc91a275d242
Raquel Lopez <raquelcle@gmail.com>
2026-03-02 22:48:57 -0400

    green: test passes that outputs 'Fizz' if input is 9

--- 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,12 +1,12 @@
 export function fizzbuzz(input: number): string {
-    if(input === 3) {
-        return 'fizz'
-    }
     if(input === 5) {
         return 'buzz'
     }
     if(input === 15) {
         return 'FizzBuzz'
+    }
+    if(input % 3 === 0) {
+        return 'fizz'
     }
     return ""
 }
