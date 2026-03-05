c5eeed16feb94f70fe6a87ccc1c04a5a5d36bd4d
Raquel Lopez <raquelcle@gmail.com>
2026-03-05 10:55:43 -0400

    refactor(palindromeChecker): consolidate true test cases using parameterized tests

--- 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/palindrome.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/palindrome.spec.ts
@@ -1,21 +1,21 @@
 import {describe, it, expect} from 'bun:test'
 import {palindromeChecker} from "./index";
 
+const truePalindromeCases = [
+    'mom',
+    'Mom',
+    '1Never Odd or Even1'
+]
+
 describe('PalindromeChecker', () => {
-    it("should return true if input is mom", () => {
-        const res = palindromeChecker('mom');
-        expect(res).toBeTrue()
+    it.each(truePalindromeCases)("should return true if input is %p", (input) => {
+        const res = palindromeChecker(input);
+        expect(res).toBeTrue();
     })
+
     it("should return false if input is 'Never Odd or Even1'", () => {
         const res = palindromeChecker('Never Odd or Even1');
         expect(res).toBeFalse()
     })
-    it('should return true if input is "1Never Odd or Even1"', () => {
-        const res = palindromeChecker('1Never Odd or Even1');
-        expect(res).toBeTrue()
-    })
-    it('should return true if input is "MoM"', () => {
-        const res = palindromeChecker('MoM');
-        expect(res).toBeTrue()
-    });
+
 });
