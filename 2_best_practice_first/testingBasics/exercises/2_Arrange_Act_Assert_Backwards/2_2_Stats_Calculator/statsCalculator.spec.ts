583a720dcc0f75e45cf2a956a6e1c0301727d34a
Raquel Lopez <raquelcle@gmail.com>
2026-03-07 21:36:13 -0400

    test(statsCalculator): add tests for minimum value with multiple input sequences

--- 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/statsCalculator.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/statsCalculator.spec.ts
@@ -2,12 +2,27 @@ import {describe, it, expect} from 'bun:test';
 import {statsCalculator} from "./src/statsCalculator";
 
 
-
 describe('Stats Calculator', () => {
-    const sequence =  [2, 4, 21, -8, 53, 40];
-    it('should know the minium value is -8', () => {
-        const res = statsCalculator(sequence)
-        expect(res.minimum).toBe(-8);
-    });
+    describe('for sequence [2, 4, 21, -8, 53, 40]', () => {
+        const sequence = [2, 4, 21, -8, 53, 40];
+        it('should know the miniumm value is -8', () => {
+            const res = statsCalculator(sequence)
+            expect(res.minimum).toBe(-8);
+        });
+    })
+    describe('for sequence [100, 2, -3, -16, 1, 60, 81, 2, -3]', () => {
+        const sequence = [100, 2, -3, -16, 1, 60, 81, 2, -3];
+        it('should know the minimum value is -16', () => {
+            const res = statsCalculator(sequence)
+            expect(res.minimum).toBe(-16);
+        });
+    })
+    describe('for sequence [5, 5, 5]', () => {
+        const sequence = [5, 5, 5];
+        it('should know the minimum value is 5', () => {
+            const res = statsCalculator(sequence)
+            expect(res.minimum).toBe(5);
+        });
+    })
 
 });
