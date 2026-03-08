65942aa2ac8d99fbd02aec096c064c9b2310dc7e
Raquel Lopez <raquelcle@gmail.com>
2026-03-07 21:26:52 -0400

    test(statsCalculator): add initial test file and setup test:dev script

---
+++ 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/statsCalculator.ts
@@ -0,0 +1,37 @@
+type StatsCalculatorResponse = {
+    average: number;
+    minimum: number;
+    maximum: number;
+    elementsCount: number;
+}
+
+/**  Clarify before verify
+ * define sum, min, max and average variables;
+ * iterate over the array
+ * In each iteration check min and max by comparing the current value with the previous value
+ * at the end of the loop sum the values and divide by the length of the array /*
+ */
+
+export function statsCalculator(array: number[]): StatsCalculatorResponse {
+    let average: number = 0;
+    let minimum: number = 0;
+    let maximum: number = 0;
+    let sum: number = 0;
+    let elementsCount: number = array.length;
+
+    array.forEach((element, index) => {
+        if(index === 0) {
+            average = element;
+            minimum = element;
+            maximum = element;
+        } else {
+            if(element < minimum) {
+                minimum = element;
+            }
+        }
+    })
+
+    average = sum / elementsCount;
+
+    return {average, minimum, maximum: 0, elementsCount: 0}
+}
--- 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/statsCalculator.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/statsCalculator.spec.ts
@@ -1,5 +1,13 @@
-import {describe, it, expect} from 'bun:test'
+import {describe, it, expect} from 'bun:test';
+import {statsCalculator} from "./src/statsCalculator";
+
+
 
 describe('Stats Calculator', () => {
+    const sequence =  [2, 4, 21, -8, 53, 40];
+    it('should know the minium value is -8', () => {
+        const res = statsCalculator(sequence)
+        expect(res.minimum).toBe(-8);
+    });
 
 });
