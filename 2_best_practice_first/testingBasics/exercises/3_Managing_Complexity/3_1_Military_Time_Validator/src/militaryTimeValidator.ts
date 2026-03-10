2fda564e1daf892ac4608fb8a63f0a0ce2d03a1f
Raquel Lopez <raquelcle@gmail.com>
2026-03-10 18:49:36 -0400

    refactor(militaryTimeValidator): extract regex logic into reusable function `isValid24Time`

--- 2_best_practice_first/testingBasics/exercises/3_Managing_Complexity/3_1_Military_Time_Validator/src/militaryTimeValidator.ts
+++ 2_best_practice_first/testingBasics/exercises/3_Managing_Complexity/3_1_Military_Time_Validator/src/militaryTimeValidator.ts
@@ -1,9 +1,8 @@
-const regex24 = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+const isValid24Time = (time:string) => /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.test(time);
 
-export function militaryTimeValidator(time: string): boolean {
-    const splitTime = time.replaceAll(' ', '').split('-');
-    const [firstHour, secondHour] = splitTime;
+export function militaryTimeValidator(timeRange: string): boolean {
+    const timeRangeUnits = timeRange.replaceAll(' ', '').split('-');
+    const [startTime = '', endTime = ''] = timeRangeUnits;
 
-    // @ts-ignore
-    return !(!regex24.test(firstHour) || !regex24.test(secondHour));
+    return !(!isValid24Time(startTime) || !isValid24Time(endTime));
 }
