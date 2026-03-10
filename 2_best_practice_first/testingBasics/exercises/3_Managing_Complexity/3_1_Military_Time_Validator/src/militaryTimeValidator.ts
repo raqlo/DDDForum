6c81e63d1527d23b5df8206f27f371028a3e327f
Raquel Lopez <raquelcle@gmail.com>
2026-03-10 19:44:23 -0400

    refactor(militaryTimeValidator): extract time unit parsing into `convertTimeToUnits`

--- 2_best_practice_first/testingBasics/exercises/3_Managing_Complexity/3_1_Military_Time_Validator/src/militaryTimeValidator.ts
+++ 2_best_practice_first/testingBasics/exercises/3_Managing_Complexity/3_1_Military_Time_Validator/src/militaryTimeValidator.ts
@@ -1,19 +1,24 @@
 const isValid24Time = (time:string) => /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.test(time);
+const convertTimeToUnits = (timeRangeUnits: string) => {
+    const timeUnit = timeRangeUnits.replaceAll(' ', '').split('-');
+    return {
+        hour: timeUnit[0]!,
+        minute: timeUnit[1]!,
+    }
+}
 
 export function militaryTimeValidator(timeRange: string): boolean {
-    const timeRangeUnits = timeRange.replaceAll(' ', '').split('-');
-    const [startTime = '', endTime = ''] = timeRangeUnits;
-    const startTimeUnit = startTime.split(':')
-    const endTimeUnit = endTime.split(':')
+    const [startTime = '', endTime = ''] = timeRange.replaceAll(' ', '').split('-');
+    const startTimeUnit = convertTimeToUnits(startTime);
+    const endTimeUnit = convertTimeToUnits(endTime);
 
     if(!isValid24Time(startTime) || !isValid24Time(endTime)) {
         return false;
     }
-    if(startTimeUnit[0] === endTimeUnit[0] && startTimeUnit[1] === endTimeUnit[1]) {
+    if(startTimeUnit.hour === endTimeUnit.hour && startTimeUnit.minute === endTimeUnit.minute) {
         return false;
     }
-    // @ts-ignore
-    if(startTimeUnit[0] > endTimeUnit[0] || (startTimeUnit[0] === endTimeUnit[0] && startTimeUnit[1] > endTimeUnit[1])) {
+    if(startTimeUnit.hour > endTimeUnit.hour || (startTimeUnit.hour === endTimeUnit.hour && startTimeUnit.minute > endTimeUnit.minute)) {
         return false;
     }
     return true;
