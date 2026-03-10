acac2127f843bc0b725b2ceeb87285ef6c805c8f
Raquel Lopez <raquelcle@gmail.com>
2026-03-10 19:54:10 -0400

    refactor(militaryTimeValidator): extract reusable helper functions for cleaner logic
    - Added `splitTimeRange` for splitting input time ranges.
    - Introduced `isStartEqualThanEndTime` and `isStartLaterThanEndTime` for clearer time comparison logic.

--- 2_best_practice_first/testingBasics/exercises/3_Managing_Complexity/3_1_Military_Time_Validator/src/militaryTimeValidator.spec.ts
+++ 2_best_practice_first/testingBasics/exercises/3_Managing_Complexity/3_1_Military_Time_Validator/src/militaryTimeValidator.spec.ts
@@ -1,31 +1,41 @@
-import {describe, it, expect } from "bun:test"
+import {describe, it, expect} from "bun:test"
 import {militaryTimeValidator} from "./militaryTimeValidator";
 
+const validTimeRanges = [
+    "01:12 - 14:32",
+    "22:00 - 23:12",
+    "00:00 - 00:01",
+]
+
+const not24HourTimeRanges = [
+    "25:00 - 12:23",
+    "12:23 - 12:23",
+    "22:00 - 01:12",
+    "12:61 - 13:00",
+    "25:61 - 13:00"
+]
+
+const timeRangeNotInCorrectFormat = [
+    "12:23 - 12:23",
+    "22:00 - 01:12",
+    "12:23",
+    "11:35:00 - 12:30",
+    "18:23:00 - 20:30:00"
+]
+
 describe('Military Time Validator', () => {
-    it("should know that 01:12 - 14:32 is a valid time range", () => {
-        const range = "01:12 - 14:32";
+    it.each(validTimeRanges)("should know that %p is a valid time range", (range) => {
         const res = militaryTimeValidator(range);
         expect(res).toBeTrue();
     })
 
-    it("should know that 25:00 - 12:23 is not a valid time range", () => {
-        const range = "25:00 - 12:23";
+
+    it.each(not24HourTimeRanges)("should know that %p is not a valid 24 hour time range", (range) => {
         const res = militaryTimeValidator(range);
         expect(res).toBeFalse();
     })
 
-    it("should know that 12:23 - 12:23 is not a valid time range", () => {
-        const range = "12:23 - 12:23";
-        const res = militaryTimeValidator(range);
-        expect(res).toBeFalse();
-    })
-    it("should know that 22:00 - 01:12 is not a valid time range", () => {
-        const range = "22:00 - 01:12";
-        const res = militaryTimeValidator(range);
-        expect(res).toBeFalse();
-    })
-    it("should know that 12:23 is not a valid time range", () => {
-        const range = "12:23";
+    it.each(timeRangeNotInCorrectFormat)("should know that %p is not in the correct format", (range) => {
         const res = militaryTimeValidator(range);
         expect(res).toBeFalse();
     })
--- 2_best_practice_first/testingBasics/exercises/3_Managing_Complexity/3_1_Military_Time_Validator/src/militaryTimeValidator.ts
+++ 2_best_practice_first/testingBasics/exercises/3_Managing_Complexity/3_1_Military_Time_Validator/src/militaryTimeValidator.ts
@@ -1,4 +1,6 @@
-const isValid24Time = (time:string) => /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.test(time);
+type TimeUnit = {hour: string, minute: string}
+
+const splitTimeRange = (timeRange: string) => timeRange.replaceAll(' ', '').split('-');
 const convertTimeToUnits = (timeRangeUnits: string) => {
     const timeUnit = timeRangeUnits.replaceAll(' ', '').split('-');
     return {
@@ -6,21 +8,30 @@ const convertTimeToUnits = (timeRangeUnits: string) => {
         minute: timeUnit[1]!,
     }
 }
+const isValid24Time = (time:string) => /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.test(time);
+
+const isStartEqualThanEndTime = (start: TimeUnit , end: TimeUnit) => {
+  return start.hour === end.hour && start.minute === end.minute
+}
+
+const isStartLaterThanEndTime = (start: TimeUnit, end: TimeUnit) => {
+  return start.hour > end.hour || (start.hour === end.hour && start.minute > end.minute)
+}
 
 export function militaryTimeValidator(timeRange: string): boolean {
-    const [startTime, endTime] = timeRange.replaceAll(' ', '').split('-');
+    const [startTime, endTime] = splitTimeRange(timeRange);
     if (!startTime || !endTime) return false
 
     const startTimeUnit = convertTimeToUnits(startTime);
     const endTimeUnit = convertTimeToUnits(endTime);
 
-    if(!isValid24Time(startTime) && !isValid24Time(endTime)) {
+    if(!isValid24Time(startTime) || !isValid24Time(endTime)) {
         return false;
     }
-    if(startTimeUnit.hour === endTimeUnit.hour && startTimeUnit.minute === endTimeUnit.minute) {
+    if(isStartEqualThanEndTime(startTimeUnit, endTimeUnit)) {
         return false;
     }
-    if(startTimeUnit.hour > endTimeUnit.hour || (startTimeUnit.hour === endTimeUnit.hour && startTimeUnit.minute > endTimeUnit.minute)) {
+    if(isStartLaterThanEndTime(startTimeUnit, endTimeUnit)) {
         return false;
     }
     return true;
