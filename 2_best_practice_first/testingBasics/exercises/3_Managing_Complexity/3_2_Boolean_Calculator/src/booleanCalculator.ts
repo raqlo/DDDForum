2b54a27b739c2e5a9b401942457b6e5e1fa9d245
Raquel Lopez <raquelcle@gmail.com>
2026-03-11 17:56:54 -0400

    refactor(boolean-calculator): extract logical operators into dedicated functions

--- 2_best_practice_first/testingBasics/exercises/3_Managing_Complexity/3_2_Boolean_Calculator/src/booleanCalculator.ts
+++ 2_best_practice_first/testingBasics/exercises/3_Managing_Complexity/3_2_Boolean_Calculator/src/booleanCalculator.ts
@@ -1,19 +1,22 @@
 const parseBoolean = (input: string) => input === 'TRUE';
+const orOperator = (a: boolean, b: boolean) => a || b;
+const andOperator = (a: boolean, b: boolean) => a && b;
+const notOperator = (a: boolean) => !a;
 
 export function booleanCalculator(input: string): boolean {
     const tokens = input.split(' ');
-
-    if (tokens.length > 1) {
+    console.log(tokens);
+    if (tokens.length > 1 && tokens.length < 3) {
         if (tokens[0] === 'NOT') {
-            return !parseBoolean(tokens[1]);
+            return notOperator(parseBoolean(tokens[1]));
         }
 
         if (tokens[1] === 'OR') {
-            return parseBoolean(tokens[0]) || parseBoolean(tokens[2]);
+            return orOperator(parseBoolean(tokens[0]), parseBoolean(tokens[2]));
         }
 
         if (tokens[1] === 'AND') {
-            return parseBoolean(tokens[0]) && parseBoolean(tokens[2]);
+            return andOperator(parseBoolean(tokens[0]), parseBoolean(tokens[2]));
         }
     }
 
