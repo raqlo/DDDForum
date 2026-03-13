0c70ca8cfe83ae953b384a96a07d26415bbbaf1f
Raquel Lopez <raquelcle@gmail.com>
2026-03-12 23:41:06 -0400

    refactor(refactor-4-tier): extracted grade student assignment controller

--- 2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/src/controllers.ts
+++ 2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/src/controllers.ts
@@ -221,3 +221,43 @@ export async function SubmitStudentAssignmentController(req: Request, res: Respo
         res.status(500).json({error: Errors.ServerError, data: undefined, success: false});
     }
 }
+
+export async function GradeStudentAssignmentController(req: Request, res: Response) {
+    try {
+
+        if (isMissingKeys(req.body, ['id', 'grade'])) {
+            return res.status(400).json({error: Errors.ValidationError, data: undefined, success: false});
+        }
+
+        const {id, grade} = req.body;
+
+        // validate grade
+        if (!['A', 'B', 'C', 'D'].includes(grade)) {
+            return res.status(400).json({error: Errors.ValidationError, data: undefined, success: false});
+        }
+
+        // check if student assignment exists
+        const studentAssignment = await prisma.studentAssignment.findUnique({
+            where: {
+                id
+            }
+        });
+
+        if (!studentAssignment) {
+            return res.status(404).json({error: Errors.AssignmentNotFound, data: undefined, success: false});
+        }
+
+        const studentAssignmentUpdated = await prisma.studentAssignment.update({
+            where: {
+                id
+            },
+            data: {
+                grade,
+            }
+        });
+
+        res.status(200).json({error: undefined, data: parseForResponse(studentAssignmentUpdated), success: true});
+    } catch (error) {
+        res.status(500).json({error: Errors.ServerError, data: undefined, success: false});
+    }
+}
--- 2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/src/index.ts
+++ 2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/src/index.ts
@@ -7,7 +7,7 @@ import {
     AssignStudentToClassController,
     CreateAssignmentController,
     CreateClassController,
-    CreateStudentController, SubmitStudentAssignmentController
+    CreateStudentController, GradeStudentAssignmentController, SubmitStudentAssignmentController
 } from "./controllers";
 const cors = require('cors');
 const app = express();
@@ -64,46 +64,9 @@ app.post('/student-assignments', AssignStudentToAssignmentController);
 // POST student submitted assignment
 app.post('/student-assignments/submit', SubmitStudentAssignmentController);
 
-// POST student assignment graded
-app.post('/student-assignments/grade', async (req: Request, res: Response) => {
-    try {
 
-        if (isMissingKeys(req.body, ['id', 'grade'])) {
-            return res.status(400).json({ error: Errors.ValidationError, data: undefined, success: false });
-        }
-    
-        const { id, grade } = req.body;
-    
-        // validate grade
-        if (!['A', 'B', 'C', 'D'].includes(grade)) {
-            return res.status(400).json({ error: Errors.ValidationError, data: undefined, success: false });
-        }
-        
-        // check if student assignment exists
-        const studentAssignment = await prisma.studentAssignment.findUnique({
-            where: {
-                id
-            }
-        });
-    
-        if (!studentAssignment) {
-            return res.status(404).json({ error: Errors.AssignmentNotFound, data: undefined, success: false });
-        }
-    
-        const studentAssignmentUpdated = await prisma.studentAssignment.update({
-            where: {
-                id
-            },
-            data: {
-                grade,
-            }
-        });
-    
-        res.status(200).json({ error: undefined, data: parseForResponse(studentAssignmentUpdated), success: true });
-    } catch (error) {
-        res.status(500).json({ error: Errors.ServerError, data: undefined, success: false });
-    }
-});
+// POST student assignment graded
+app.post('/student-assignments/grade', GradeStudentAssignmentController);
 
 
 // GET all students
