3769ccb1c0196e2211961fd882a23d046e9fb93f
Raquel Lopez <raquelcle@gmail.com>
2026-03-15 21:32:30 -0400

    test(e2e): add test for class creation

---
+++ 2_best_practice_first/strategyDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/e2e/createClassroom.test.ts
@@ -0,0 +1,49 @@
+import request from "supertest";
+import { app } from "../../src";
+
+import {defineFeature, loadFeature} from "jest-cucumber";
+import path from "path";
+
+const feature = loadFeature(
+    path.join(__dirname, "../features/createClassRoom.feature")
+);
+import { resetDatabase } from "../fixtures/reset";
+
+
+defineFeature(feature, (test) => {
+    test('Successfully create a class room', ({given, when, then}) => {
+        let requestBody: any = {};
+        let response: any = {};
+
+        given(/^I want to create a class room named "(.*)"$/, (name) => {
+            requestBody = {
+                name,
+            };
+        })
+
+        when('I send a request to create a class room', async () => {
+            response = await request(app).post("/classes").send(requestBody);
+        });
+
+        then('the class room should be created successfully', () => {
+            expect(response.status).toBe(201);
+            expect(response.body.data.name).toBe(requestBody.name);
+        });
+
+
+        test('Fail to create a class room', ({given, when, then}) => {
+            given('I want to create a class room with no name', () => {
+
+            });
+
+            when('I send a request to create a class room', () => {
+
+            });
+
+            then('the class room should not be created', () => {
+
+            });
+        });
+
+    });
+})
