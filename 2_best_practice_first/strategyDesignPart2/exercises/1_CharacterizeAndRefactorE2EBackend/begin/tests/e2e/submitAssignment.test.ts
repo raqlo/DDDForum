8bf85be106b76c5089adbff6abf68bebe44b52fa
Raquel Lopez <raquelcle@gmail.com>
2026-03-24 19:25:32 -0400

    test(student-assignments-submit): add test for assignment submission successfully scenario

---
+++ 2_best_practice_first/strategyDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/e2e/submitAssignment.test.ts
@@ -0,0 +1,51 @@
+import {defineFeature, loadFeature} from "jest-cucumber";
+import path from "path";
+import {ClassBuilder} from "../fixtures/classBuilder";
+import {StudentBuilder} from "../fixtures/studentBuilder";
+import {ClassEnrollmentBuilder} from "../fixtures/classEnrollmentBuilder";
+import {AssignmentBuilder} from "../fixtures/assignmentBuilder";
+import {StudentAssignmentBuilder} from "../fixtures/studentAssignmentBuilder";
+import request from "supertest";
+import {app} from "../../src";
+import {resetDatabase} from "../fixtures/reset";
+
+const feature = loadFeature(
+    path.join(__dirname, "../features/submitAssignment.feature")
+);
+
+defineFeature(feature, (test) => {
+    beforeEach(async () => {
+        await resetDatabase()
+    })
+    test('Submits assignment', ({given, and, when, then}) => {
+        let enrollment: any = {};
+        let assignment: any = {};
+        let response: any = {};
+        let requestBody: any = {};
+        let studentAssignment: any = {};
+
+        given('That I have a student assigned to a class', async () => {
+            const classroom = new ClassBuilder().withName('Math 301')
+            const student = new StudentBuilder().withName('John Snow').withEmail('jsnow@email.com')
+            enrollment = await new ClassEnrollmentBuilder().withClass(classroom).withStudent(student).build()
+        });
+
+        and('That student has assigned an assignment', async () => {
+            assignment = new AssignmentBuilder().withClassId(enrollment.classId).withTitle('Math Assignment')
+            studentAssignment = await new StudentAssignmentBuilder().withAssignment(assignment).withStudentId(enrollment.studentId).build()
+        });
+
+        when('the student submits their assignment', async () => {
+            requestBody = {
+                assignmentId: studentAssignment.assignmentId,
+                studentId: studentAssignment.studentId,
+            }
+            response = await request(app).post('/student-assignments/submit').send(requestBody)
+        });
+
+        then('An assignment submission is created', () => {
+            expect(response.status).toBe(201);
+        });
+
+    })
+})
---
+++ 2_best_practice_first/strategyDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/features/submitAssignment.feature
@@ -0,0 +1,10 @@
+Feature: Submit an Assignment
+  As a student
+  I want to submit an assignment
+  So that I can get a grade
+
+  Scenario: Submits assignment
+    Given That I have a student assigned to a class
+    And That student has assigned an assignment
+    When the student submits their assignment
+    Then An assignment submission is created
