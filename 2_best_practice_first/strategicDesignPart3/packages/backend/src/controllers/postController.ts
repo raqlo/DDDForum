c8203d6775d80b47a80011527b830ec60161e113
Raquel Lopez <raquelcle@gmail.com>
2026-04-05 20:59:12 -0400

    refactor(backend): extract `PostController` and integrate it into `index.ts`
    - Move post-related logic to a new `PostController` for improved modularity.
    - Update `index.ts` to use `postController` for handling `/posts` route.
    
    Signed-off-by: Raquel Lopez <raquelcle@gmail.com>

---
+++ 2_best_practice_first/strategicDesignPart3/packages/backend/src/controllers/postController.ts
@@ -0,0 +1,35 @@
+// Get posts
+import {Request, Response} from "express";
+import {Errors} from "../shared/errors";
+import {prisma} from "../database";
+
+export class PostController {
+    async getPosts(req: Request, res: Response) {
+        try {
+            const {sort} = req.query;
+
+            if (sort !== 'recent') {
+                return res.status(400).json({error: Errors.ClientError, data: undefined, success: false})
+            }
+
+            let postsWithVotes = await prisma.post.findMany({
+                include: {
+                    votes: true, // Include associated votes for each post
+                    memberPostedBy: {
+                        include: {
+                            user: true
+                        }
+                    },
+                    comments: true
+                },
+                orderBy: {
+                    dateCreated: 'desc', // Sorts by dateCreated in descending order
+                },
+            });
+
+            return res.json({error: undefined, data: {posts: postsWithVotes}, success: true});
+        } catch (error) {
+            return res.status(500).json({error: Errors.ServerError, data: undefined, success: false});
+        }
+    }
+}
--- 2_best_practice_first/strategicDesignPart3/packages/backend/src/index.ts
+++ 2_best_practice_first/strategicDesignPart3/packages/backend/src/index.ts
@@ -1,8 +1,7 @@
-import express, {Request, Response} from 'express';
-import {prisma} from './database';
+import express from 'express';
 import {UserController} from "./controllers/userController";
-import {Errors} from "./shared/errors";
 import {MarketingController} from "./controllers/marketingController";
+import {PostController} from "./controllers/postController";
 
 const cors = require('cors')
 const app = express();
@@ -11,6 +10,7 @@ app.use(cors())
 
 const userController = new UserController();
 const marketingController = new MarketingController();
+const postController = new PostController()
 
 app.post('/users/new', userController.createUserAccount);
 
@@ -18,36 +18,7 @@ app.post('/marketing/new', marketingController.addUserToMarketingList)
 
 app.get('/users', userController.getUsers);
 
-// Get posts
-const getPostList = async (req: Request, res: Response) => {
-  try {
-    const { sort } = req.query;
-
-    if (sort !== 'recent') {
-      return res.status(400).json({ error: Errors.ClientError, data: undefined, success: false })
-    }
-
-    let postsWithVotes = await prisma.post.findMany({
-      include: {
-        votes: true, // Include associated votes for each post
-        memberPostedBy: {
-          include: {
-            user: true
-          }
-        },
-        comments: true
-      },
-      orderBy: {
-        dateCreated: 'desc', // Sorts by dateCreated in descending order
-      },
-    });
-
-    return res.json({ error: undefined, data: { posts: postsWithVotes }, success: true });
-  } catch (error) {
-    return res.status(500).json({ error: Errors.ServerError, data: undefined, success: false });
-  }
-};
-app.get('/posts', getPostList);
+app.get('/posts', postController.getPosts);
 
 // prisma.post.findMany({})
 //   .then((posts) => console.log(posts))
