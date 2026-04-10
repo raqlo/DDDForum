58fd5e5708eb69561751e87ff566e3d5e3c065d9
Raquel Lopez <raquelcle@gmail.com>
2026-04-10 11:41:27 -0400

    refactor(frontend): modularize shared `API` client and integrate with `RegisterPage`
    - Introduce shared `API` client for `Users`, `Marketing`, and `Posts` under `packages/shared/src/api`.
    - Define type-safe API responses, error types, and client methods for improved error handling and data validation.
    - Update `RegisterPage` to use the shared `API` client, providing more robust error handling for user registration.
    
    Signed-off-by: Raquel Lopez <raquelcle@gmail.com>

--- 2_best_practice_first/strategicDesignPart3/packages/frontend/src/pages/registerPage.tsx
+++ 2_best_practice_first/strategicDesignPart3/packages/frontend/src/pages/registerPage.tsx
@@ -5,7 +5,7 @@ import {
   RegistrationInput,
 } from "../components/registrationForm";
 import { ToastContainer, toast } from 'react-toastify';
-import { api } from "../api";
+import { createAPIClient } from "../../../shared/src/api";
 import { useUser } from "../contexts/userContext";
 import { useNavigate } from "react-router-dom";
 import { useSpinner } from "../contexts/spinnerContext";
@@ -27,6 +27,7 @@ export const RegisterPage = () => {
   const { setUser } = useUser();
   const navigate = useNavigate()
   const spinner = useSpinner();
+  const api = createAPIClient("http://localhost:3000");
 
   const handleSubmitRegistrationForm = async (input: RegistrationInput) => {
     // Validate the form
@@ -43,10 +44,24 @@ export const RegisterPage = () => {
     spinner.activate();
     try {
       // Make API call
-      const response = await api.register(input);
-      // Save the user details to the cache
-      setUser(response.data.data);
-      console.log('setting data', response.data.data)
+      const response = await api.users.register(input);
+      if (!response.success) {
+        switch (response.error.code){
+          case 'EmailAlreadyInUse':
+            return toast.error('This email is already in use. Perhaps you want to log in?');
+          case 'UsernameAlreadyTaken':
+            return toast.error('Please try a different username, this one is already taken.');
+          case 'ValidationError':
+            // We could further improve this with more refined types
+            // to specify which form field was invalid.
+            return toast.error(response.error.message);
+          case "ServerError":
+          default:
+            return toast.error('Some backend error occurred');
+        }
+      }
+
+      setUser(response.data);
       // Stop the loading spinner
       spinner.deactivate();
       // Show the toast
@@ -60,9 +75,7 @@ export const RegisterPage = () => {
       // Show the toast (for unknown error)
       return toast.error('Some backend error occurred');
     }
-
   };
-
   return (
     <Layout>
       <ToastContainer/>
